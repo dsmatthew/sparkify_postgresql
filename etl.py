2eadb1dcb6876b24d86acb355c6be78e32f4bb0d
Matthew <9074897+dsmatthew@users.noreply.github.com>
2020-11-26 22:40:36 +0100

    Update etl.py
    follow fix from the SQL-query fixes

--- etl.py
+++ etl.py
@@ -25,14 +25,14 @@ def process_song_file(cur, filepath):
     
     '''
     # open song file
-    df = pd.read_json(filepath, typ='series')
+    df = pd.read_json(filepath, lines=True, orient='columns')
 
     # insert song record
-    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[:].tolist()
+    song_data = df[['song_id', 'title', 'artist_id', 'year', 'duration']].values[0].tolist()
     cur.execute(song_table_insert, song_data)
     
     # insert artist record
-    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[:].tolist()
+    artist_data = df[['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']].values[0].tolist()
     cur.execute(artist_table_insert, artist_data)
 
 
@@ -76,7 +76,7 @@ def process_log_file(cur, filepath):
 
     # load user table
     user_df = df[['userId', 'firstName', 'lastName', 'gender', 'level']]
-    user_df = user_df.replace('', np.nan) # fix for the string
+    #user_df = user_df.replace('', np.nan) # fix for the string
 
     # insert user records
     for i, row in user_df.iterrows():
@@ -95,7 +95,7 @@ def process_log_file(cur, filepath):
             songid, artistid = None, None
 
         # insert songplay record
-        songplay_data = (index, row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
+        songplay_data = (row.ts, row.userId, row.level, songid, artistid, row.sessionId, row.location, row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
